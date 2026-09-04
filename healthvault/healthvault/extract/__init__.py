d41150d701ed7a8a11ee7d8f8607274089214f77
avennaidu <aven@avemel.com>
2026-09-04 14:42:22 +0200

    Delete healthvault directory

--- healthvault/healthvault/extract/__init__.py
+++
@@ -1,4 +0,0 @@
-"""Turning files into text, and text into a guess about what it is."""
-
-from .text import extract_text, sha256_of          # noqa: F401
-from .classify import classify, find_observations  # noqa: F401
